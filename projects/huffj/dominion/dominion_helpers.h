aeec3bd9effdfd507d3cd504fc1996ff650f4af9
josh <josh@e-huff.com>
2018-04-17 13:35:48 -0700

    included card functions in dominion_helpers.h

--- projects/huffj/dominion/dominion_helpers.h
+++ projects/huffj/dominion/dominion_helpers.h
@@ -11,5 +11,9 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
+int card_adventurer(struct gameState *state, int currentPlayer);
+int card_council_room(struct gameState *state, int currentPlayer, int handPos);
+int card_remodel(struct gameState *state, int currentPlayer, int choice1, int choice2, int handPos);
+int card_smithy(struct gameState *state, int currentPlayer, int handPos);
 
 #endif
