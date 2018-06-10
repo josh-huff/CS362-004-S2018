96f2a320eb2b4f079de6b5a3505ed7e07d0734ac
josh-huff <huffj@oregonstate.edu>
2018-06-10 13:43:40 -0700

    "paranoia"

--- projects/huffj/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/huffj/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -4,31 +4,37 @@ import junit.framework.TestCase;
 
 public class UrlValidatorTest extends TestCase {
 
-    // Valid schemes are described in RFC 3986
-    // Because a scheme is optional, the array starts with the empty string.
-/*
+    /*
 
-John Eleen found a bug that precludes all valid schemes besides http.
-If he had not, this would be the validScheme array passed to our unit test. Instead, it is below.
+        ---- IMPORTANT NOTE ----
 
-   String[] validScheme =  { "scheme",
-           //"",
-           "http://",  "https://", "ftp://", "ftps://",
-           "http+://", "ht-tp://", "h.t.t.p.://"
-   };
-*/
-    String[] validScheme = {"scheme",
-            "http://"
-    };
+    John Eleen found a bug when performing input partition.
+    The bug precludes all valid schemes besides http and all valid port numbers in authorities.
+    If he had not, this would be the validScheme array passed to our unit test.
+
+        // Valid schemes are described in RFC 3986
+        // Because a scheme is optional, the array starts with the empty string.
+       String[] validScheme =  { "scheme",
+               //"",
+               "http://",  "https://", "ftp://", "ftps://",
+               "http+://", "ht-tp://", "h.t.t.p.://"
+       };
+    */
+    String[] validScheme = {"scheme", "http://"};
 
     // Invalid schemes start with a numeric or contain invalid characters. These are a small sample of what should fail.
-    String[] invalidScheme = {"0http://", "http^://"};
+    String[] invalidScheme = {"scheme", "0http://", "http^://"};
 
+    // Valid Authorities are described in RFC 3986
+    String[] validAuth = {"authority", "google.com", "255.255.255.255", "google.co.uk"};
+
+    // Invalid authorities are empty strings, octets and ports out of range, and invalid top level domains
+    String[] invalidAuth = {"authority", "", "256.255.255.255", "google.255"};
 
     // This array's elements each contain a different valid path character
     // Valid path characters can be found in RFC 3986
     // Because a path is optional, the array starts with the empty string.
-    String[] validPath = {"",
+    String[] validPath = { "path", "",
             "/pathtest", "/PATHTEST", "/pathtest01", "/path.test",
             "/path-test", "/path_test", "/path~test", "/path!test",
             "/path$test", "/path&test", "/path'test", "/path(test",
@@ -37,22 +43,12 @@ If he had not, this would be the validScheme array passed to our unit test. Inst
     };
 
     // Invalid paths contain characters not in that subset. These are a small sample of what should fail.
-    String[] invalidPath = {"/path|test", "/path#test", "/path^test", "/path`test"};
-
-    // Valid Authorities are described in RFC 3986
-    String[] validAuth = {"google.com", "google.com:80", "255.255.255.255", "google.co.uk"};
-
-    // Invalid authorities are empty strings, octets and ports out of range, and invalid top level domains
-    String[] invalidAuth = {"", "256.255.255.255", "google.com:70000", "google.255"};
+    String[] invalidPath = {"path", "/path|test", "/path#test", "/path^test", "/path`test"};
 
     // Huff's contribution: programming-based testing
     public void testIsValid() {
 
-//        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-
-        String compStr;
-//        String[][] valids = {validScheme, validPath, validAuth};
-        String[][] valids = {validScheme}; // FOR TESTING ONLY
+        String[][] valids = {validScheme, validAuth, validPath}; // FOR TESTING ONLY
 
         // Counters to track which subsets of the URL cause failures.
         // Pattern fails shouldn't be possible; using a composite string forces the URL to follow the pattern.
@@ -86,12 +82,42 @@ If he had not, this would be the validScheme array passed to our unit test. Inst
                             fragmentFails++;
                             break;
                         default:
-                            System.out.println("Problem with switch statement in testIsValid()");
+                            System.out.println("Problem with switch statement in testIsValid()'s valids loop");
                     }
                 }
             }
         }
+/*
+        for (int i = 0; i < invalids.length; i++) {
 
+            for (int j = 1; j < invalids[i].length; j++) {
+
+                if (!accepted(invalids[i], invalids[i][j], j, "rejected")) {
+
+                    switch (invalids[i][0]) {
+
+                        case "scheme":
+                            schemeFails++;
+                            break;
+                        case "authority":
+                            authorityFails++;
+                            break;
+                        case "path":
+                            pathFails++;
+                            break;
+                        case "query":
+                            queryFails++;
+                            break;
+                        case "fragment":
+                            fragmentFails++;
+                            break;
+                        default:
+                            System.out.println("Problem with switch statement in testIsValid()'s invalids loop");
+                    }
+                }
+            }
+        }
+*/
         // Print totals of failures
         System.out.println("Number of scheme failures: " + Integer.toString(schemeFails));
         System.out.println("Number of authority failures: " + Integer.toString(authorityFails));
@@ -117,9 +143,16 @@ If he had not, this would be the validScheme array passed to our unit test. Inst
 
         if (componentType.equals("authority")) {
 
-            knownValid = "google.com";
-            compStr = element + knownValid;
+            knownValid = "http://";
+            compStr = knownValid + element;
         }
+
+        if (componentType.equals("path")) {
+
+            knownValid = "http://";
+            compStr = knownValid + element;
+        }
+
 
         if (expected.equals("accepted")) {
 
